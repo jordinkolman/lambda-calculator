4b6111bb18b1d60c2d385b19f88890911f982876
Jordin Kolman <git@github.com>
2019-07-16 18:02:33 -0500

    Pushing current edits for airtable

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //import any components needed
+import NumberButton from './NumberButton.js'
 
 //Import your array data to from the provided data file
+import { numbers } from '../../../data.js'
 
-const Numbers = () => {
+export const Numbers = () => {
+  const [numberState] = useState(numbers)
   // STEP 2 - add the imported data to state
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       
     </div>
   );
+    
 };
+
+
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import operators from '../../../data.js'
 
 //import any components needed
 
@@ -6,6 +7,7 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorsState] = useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
